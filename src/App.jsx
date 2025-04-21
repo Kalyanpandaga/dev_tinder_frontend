0571479fc63227711624a58eb32658dc555287c9
kalyan <kalyanlee3@gmail.com>
2025-04-21 20:53:41 +0530

    add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
+import { Route, Routes } from "react-router";
 import "./App.css";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
+import Testing from "./components/Testing";
+import Body from "./components/Body";
 
 function App() {
   return (
-    <>
-      <div>
-        <h1> Hello world </h1>
-      </div>
-    </>
+    <Routes>
+      <Route path="/" element={<Body />}>
+        <Route path="login" element={<Login />} />
+        <Route path="profile" element={<Profile />} />
+        <Route index path="/" element={<Testing />} />
+      </Route>
+    </Routes>
   );
 }
 
